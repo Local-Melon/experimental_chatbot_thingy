ca1fc3788de4f1cf6e4464c15d646b6a728e9605
Local-Melon <asdfqwertyy9@gmail.com>
2024-12-08 15:55:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,50 +8,50 @@ def chat(client):
     I want you to turn this into Old English, Middle English, Early Modern English and Modern English.
     The response should only include the type and the sentences
     It should look like this -> Old English: "Sentence" """
-    agree2 = st.checkbox("Sentence Inserted")
-    if agree2:
-        messages_so_far = [
-            {"role": "user", "content": prompt}
-            ]
+    messages_so_far = [
+        {"role": "user", "content": prompt}
+    ]
 
-        response = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=messages_so_far,
-            temperature=0.8,
-            )
-        return response.choices[0].message.content
-    else:
-        pass
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=messages_so_far,
+        temperature=0.8,
+        )
+    return response.choices[0].message.content
 
 
 client = openai.OpenAI(api_key=st.text_input("OpenAI API Key", key="chatbot_api_key", type="password"))
 
-agree = st.checkbox("Api Key Inputted")
+agree = st.checkbox("Api Key Inserted")
 if agree:
     output = chat(client)
-    output_list = output.split('"')
-    key_list = []
-    value_list = []
-    list_index = 0
-    for element in output_list:
-        if len(element)!=0:
-            for i in range(5):
-                element = element.lstrip(' ')
-            for i in range(5):
-                element = element.strip(' ')
-        element = element.lstrip('\n')
-        element = element.strip('\n')
-        element = element.lstrip(':')
-        element = element.strip(':')
-        if element != '':
-            if list_index %2 == 0:
-                key_list.append(element)
-            else:
-                value_list.append(element)
-            list_index +=1
-    display_dict = {"Type": key_list, "Sentence": value_list}
-    df = pd.DataFrame(display_dict)
+    agree2 = st.checkbox("Sentence Inserted")
+    if agree2 :
+        output_list = output.split('"')
+        key_list = []
+        value_list = []
+        list_index = 0
+        for element in output_list:
+            if len(element)!=0:
+                for i in range(5):
+                    element = element.lstrip(' ')
+                for i in range(5):
+                    element = element.strip(' ')
+            element = element.lstrip('\n')
+            element = element.strip('\n')
+            element = element.lstrip(':')
+            element = element.strip(':')
+            if element != '':
+                if list_index %2 == 0:
+                    key_list.append(element)
+                else:
+                    value_list.append(element)
+                list_index +=1
+        display_dict = {"Type": key_list, "Sentence": value_list}
+        df = pd.DataFrame(display_dict)
 
-    df
+        df
+    else:
+        pass
 else:
     pass
